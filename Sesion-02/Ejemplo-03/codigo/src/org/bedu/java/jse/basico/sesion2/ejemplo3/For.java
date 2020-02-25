7f09d424eff3c7a24900e4bcaea641e37bf6465f
Alex Montoya <amonmar2000@hotmail.com>
2020-02-24 22:15:41 -0600

    Se modifican las instrucciones del ejemplo 3 y se elimina el reto 3

--- Sesion-02/Ejemplo-03/codigo/src/org/bedu/java/jse/basico/sesion2/ejemplo3/For.java
+++ Sesion-02/Ejemplo-03/codigo/src/org/bedu/java/jse/basico/sesion2/ejemplo3/For.java
@@ -12,12 +12,24 @@ public class For {
             //System.out.println("El valor actual del arreglo es " + valores[i]);
         }
 
-        for (int i = 1; i < valores.length; i+=2) {
+        for (int i = 1; i < valores.length; i += 2) {
             //System.out.println("El valor actual del arreglo es " + valores[i]);
         }
 
-        for(short valor : valores){
-            System.out.println("El valor actual del arreglo es " + valor);
+        for (short valor : valores) {
+            //System.out.println("El valor actual del arreglo es " + valor);
         }
+
+        short i = 0;
+
+        while (++i <= 10) {
+            //System.out.println("El valor de i es " + i);
+        }
+
+        i = 1;
+
+        do {
+            System.out.println("El valor de i es " + i);
+        } while (i++ < 10);
     }
 }
